1f8ccb5523f690c5172588bd55048824c907afa2
Morris235 <dltkdah777@gmail.com>
2025-09-14 13:21:09 +0900

    fix : error

--- src/playground/TestCode.java
+++ src/playground/TestCode.java
@@ -3,13 +3,3 @@ package playground;
 public class TestCode {
     
 }
-
-public int fn (int n) {
-    int r = n;
-    for (int i = 0; i < n; i++) {
-        while(n > 1)
-            n /= 2;
-        n = r;
-    }
-    return r;
-}
